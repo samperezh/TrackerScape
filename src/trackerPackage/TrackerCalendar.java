3897f00082d3bdeb61e3e9619500fac0c7afed0d
Samantha Perez Hoffman <68136998+samperezh@users.noreply.github.com>
2021-11-06 17:11:49 -0400

    Changes in TrackerCalendar

--- src/trackerPackage/TrackerCalendar.java
+++ src/trackerPackage/TrackerCalendar.java
@@ -1,12 +1,18 @@
 package trackerPackage;
 
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class TrackerCalendar {
 
 	GraphicsProgram GProgram;
 	
+	
 	public TrackerCalendar (GraphicsProgram GProgram) {
+		this.GProgram = GProgram;
 		
+		GRect[] calendarCases = new GRect[35];
+		
+		//Setting up Calendar boxes 
 	}
 }
