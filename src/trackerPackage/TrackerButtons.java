705f1c1bc181a3bb47430c4b9e7500b3b3dbf4fe
naziaC <nazia.chowdhury06@gmail.com>
2021-11-06 18:52:04 -0400

    Added months and days (JComboBox)

--- src/trackerPackage/TrackerButtons.java
+++ src/trackerPackage/TrackerButtons.java
@@ -4,6 +4,7 @@ import acm.program.*;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.*;
+import static trackerPackage.TrackerParams.*;
 
 public class TrackerButtons {
 	
@@ -12,7 +13,19 @@ public class TrackerButtons {
 		
 	
 		GProgram.add(new JLabel("TrackerScape"), BorderLayout.NORTH);
-	
+		
+		JComboBox MonthChooser = new JComboBox(months);
+		MonthChooser.setEditable(false);
+		
+		GProgram.add(MonthChooser, BorderLayout.SOUTH);
+		
+		JComboBox DayChooser = new JComboBox(days);
+		DayChooser.setEditable(false);
+		
+		GProgram.add(DayChooser, BorderLayout.SOUTH);
+		
 	}
+	
+
 	
 }
--- src/trackerPackage/TrackerSim.java
+++ src/trackerPackage/TrackerSim.java
@@ -2,6 +2,7 @@ package trackerPackage;
 
 import acm.program.GraphicsProgram;
 import static trackerPackage.TrackerParams.*;
+import java.awt.Component;
 // Hello
 //hello hello
 //hiiiii
@@ -13,8 +14,8 @@ public class TrackerSim extends GraphicsProgram {
 	
 	public void run() {
 		
-		this.resize(WIDTH+OFFSET, HEIGHT+OFFSET);
-		//TrackerCalendar Calendar = new TrackerCalendar(this);
+		this.resize(TrackerParams.WIDTH+OFFSET, TrackerParams.HEIGHT+OFFSET);
+		TrackerCalendar Calendar = new TrackerCalendar(this);
 		TrackerButtons Buttons = new TrackerButtons(this);
 		
 		
