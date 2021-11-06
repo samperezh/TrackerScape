81c5ae7f3b3198b05a8a8d8204b1b84c518560d4
Samantha Perez Hoffman <68136998+samperezh@users.noreply.github.com>
2021-11-06 17:02:25 -0400

    Setting up TrackerCalendar

--- src/trackerPackage/TrackerCalendar.java
+++ src/trackerPackage/TrackerCalendar.java
@@ -1,5 +1,12 @@
 package trackerPackage;
 
+import acm.program.GraphicsProgram;
+
 public class TrackerCalendar {
 
+	GraphicsProgram GProgram;
+	
+	public TrackerCalendar (GraphicsProgram GProgram) {
+		
+	}
 }
--- src/trackerPackage/TrackerSim.java
+++ src/trackerPackage/TrackerSim.java
@@ -1,9 +1,19 @@
 package trackerPackage;
 
+import acm.program.GraphicsProgram;
 
 // Hello
 //hello hello
 //hiiiii
-public class TrackerSim {
+public class TrackerSim extends GraphicsProgram {
 	
+	public static void main(String[] args) {
+		new TrackerSim().start(args);
+	}
+	
+	public void run() {
+		
+		TrackerCalendar Calendar = new TrackerCalendar(this);
+		
+	}
 }
